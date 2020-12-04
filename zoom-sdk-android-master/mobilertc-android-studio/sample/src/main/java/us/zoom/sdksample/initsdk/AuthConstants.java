ca612ec4050213ecc5b5fc228dd5486fdbbbd858
nero1342 <heathcliff1342@gmail.com>
2020-12-04 12:14:57 +0700

    Zoom SDK

---
+++ zoom-sdk-android-master/mobilertc-android-studio/sample/src/main/java/us/zoom/sdksample/initsdk/AuthConstants.java
@@ -0,0 +1,25 @@
+package us.zoom.sdksample.initsdk;
+
+public interface AuthConstants {
+
+	// TODO Change it to your web domain
+	public final static String WEB_DOMAIN = "zoom.us";
+
+	/**
+	 * We recommend that, you can generate jwttoken on your own server instead of hardcore in the code.
+	 * We hardcore it here, just to run the demo.
+	 *
+	 * You can generate a jwttoken on the https://jwt.io/
+	 * with this payload:
+	 * {
+	 *
+	 *     "appKey": "string", // app key
+	 *     "iat": long, // access token issue timestamp
+	 *     "exp": long, // access token expire time
+	 *     "tokenExp": long // token expire time
+	 * }
+	 */
+	public final static String SDK_KEY = "DymeOSFIbfNPCyIMv23EmmyWvYPNFJ44BPkQ";
+	public final static String SDK_SECRET = "t191t46bV8VkZqjeQdzfIOzPcYcWWpUFhuNY";
+
+}
